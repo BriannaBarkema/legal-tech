1a1a0ab66cf6bb5e187aaff57155b07284d30caa
BriannaBarkema <145512323+BriannaBarkema@users.noreply.github.com>
2024-03-30 16:32:09 -0400

    Added Examples to tenentDB.sql
    Examples added for clarity for running commands to fill tables with data.

--- tenentDB.sql
+++ tenentDB.sql
@@ -30,3 +30,14 @@ CREATE TABLE IF NOT EXISTS Prompts (
 	ResultID3 INT,
 	ResultID4 INT
 );
+-- Ex. Inserting a user with a lease file
+    --INSERT INTO Users (Name, DoB, Address, AptUnit, StateID, LeaseFile, RegistrationDate)
+    --VALUES ('John Doe', '1990-05-15', '123 Main St', 'Apt 101', '654M920', FILE_READ(“mylease.pdf”), '2024-03-30');
+--Ex. Inserting a prompt with all options and result IDs
+    --INSERT INTO Prompts (PromptName, Description, Option1, Option2, Option3, Option4, ResultID1, ResultID2, ResultID3, ResultID4)
+    --VALUES ('Favorite Color', 'What is your favorite color?', 'Red', 'Blue', 'Green', 'Other', 1, 2, 3, 4);
+--Ex. Inserting a prompt with fewer options and result IDs
+   --INSERT INTO Prompts (PromptName, Description, Option1, Option2, ResultID1, ResultID2)
+   --VALUES ('Preferred Music Genre', 'What is your preferred music genre?', 'Rock', 'Pop', 1, 2);
+
+
